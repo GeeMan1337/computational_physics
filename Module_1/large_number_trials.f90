1c4f856eb652bbbcb9369a52f2bbec64d2a01c47
GeeMan1337 <thevenomwarrior@gmail.com>
2025-01-10 00:07:42 +0530

    graph data upto Question 1i

--- Module_1/large_number_trials.f90
+++ Module_1/large_number_trials.f90
@@ -1,3 +1,5 @@
+!!! graph_data subroutine in the end 
+
 program large_number_trials
     implicit none
 
@@ -15,12 +17,16 @@ program large_number_trials
         sum_list(i)=sum(rand_num)
     end do
 
-    open(unit=1, file="E:\computational_physics\Module_1_out\question_1h.csv")
+    !open(unit=1, file="E:\computational_physics\Module_1_out\question_1h.csv")
     
-    do i=1,10000
-        write (1,*) sum_list(i)
-    end do
-    close(1)
+    !do i=1,10000
+    !    write (1,*) sum_list(i)
+    !end do
+    !close(1)
+
+    call graph_data(sum_list,10000,0.5,"E:\computational_physics\Module_1_out\graph_data_1h_1.dat",11)
+    call graph_data(sum_list,10000,1.0,"E:\computational_physics\Module_1_out\graph_data_1h_2.dat",12)
+    call graph_data(sum_list,10000,2.0,"E:\computational_physics\Module_1_out\graph_data_1h_3.dat",13)
 
     !!! 10^4 trials with 10^4 random numbers between -1 and 1 
     do i=1,10000
@@ -35,12 +41,14 @@ program large_number_trials
         sum_list(i)=sum(rand_num)
     end do
 
-    open(unit=2, file="E:\computational_physics\Module_1_out\question_1h_part_b.csv")
+    !open(unit=2, file="E:\computational_physics\Module_1_out\question_1h_part_b.csv")
     
-    do i=1,10000
-        write (2,*) sum_list(i)
-    end do
-    close(2)
+    !do i=1,10000
+    !    write (2,*) sum_list(i)
+    !end do
+    !close(2)
+
+    call graph_data(sum_list,10000,2.0,"E:\computational_physics\Module_1_out\graph_data_1h_4.dat",14)
 
     !!! 10^5 trials with 10^4 random numbers between -1 and 1 (10 times the previous one)
     do i=1,100000
@@ -55,12 +63,14 @@ program large_number_trials
         sum_bigger_list(i)=sum(rand_num)
     end do
 
-    open(unit=3, file="E:\computational_physics\Module_1_out\question_1h_part_c.csv")
+    !open(unit=3, file="E:\computational_physics\Module_1_out\question_1h_part_c.csv")
     
-    do i=1,100000
-        write (3,*) sum_bigger_list(i)
-    end do
-    close(3)
+    !do i=1,100000
+    !    write (3,*) sum_bigger_list(i)
+    !end do
+    !close(3)
+
+    call graph_data(sum_bigger_list,100000,2.0,"E:\computational_physics\Module_1_out\graph_data_1h_5.dat",15)
 
     !!! question 1i
     !!! 10^4 trials with 10^4 random numbers either -1 and 1
@@ -77,12 +87,15 @@ program large_number_trials
         sum_list(i)=sum(rand_num)
     end do
 
-    open(unit=4, file="E:\computational_physics\Module_1_out\question_1i.csv")
+    !open(unit=4, file="E:\computational_physics\Module_1_out\question_1i.csv")
     
-    do i=1,10000
-        write (4,*) sum_list(i)
-    end do
-    close(4)
+    !do i=1,10000
+    !    write (4,*) sum_list(i)
+    !end do
+    !close(4)
+
+    call graph_data(sum_list,10000,1.0,"E:\computational_physics\Module_1_out\graph_data_1i_1.dat",16)
+
 
     !!! question 1k
     !!! 10^5 trials with 10^5 random numbers either -1 and 1
@@ -99,11 +112,64 @@ program large_number_trials
         sum_bigger_list(i)=sum(rand_bigger_num)
     end do
 
-    open(unit=5, file="E:\computational_physics\Module_1_out\question_1k.csv")
+    !open(unit=5, file="E:\computational_physics\Module_1_out\question_1k.csv")
     
-    do i=1,100000
-        write (5,*) sum_bigger_list(i)
-    end do
-    close(5)
+    !do i=1,100000
+    !    write (5,*) sum_bigger_list(i)
+    !end do
+    !close(5)
+
+    call graph_data(sum_bigger_list,100000,1.0,"E:\computational_physics\Module_1_out\graph_data_1i_3.dat",17)
 
 end program large_number_trials
+
+!!! code to write bins and frequency for histogram
+subroutine graph_data(data_array, data_size, bin_size, path, unit_num)
+    implicit none
+
+    character(len=*), intent(in) :: path
+    integer :: i, j
+    integer :: num_bins, min_val, max_val
+    integer, intent(in) :: data_size, unit_num
+    real, allocatable :: bins(:), frequency(:)
+    real, intent(in) :: bin_size
+    real *8, intent(in) :: data_array(data_size)
+
+    min_val=int(minval(data_array)-bin_size)
+    max_val=int(maxval(data_array)+1.0)
+
+    if (mod(real(max_val-min_val),bin_size) /= 0.0) then
+        max_val=int(real(max_val)-mod(real(max_val-min_val),bin_size)+bin_size)
+    end if
+
+    num_bins=int(real(max_val-min_val)/bin_size)
+
+    allocate(bins(num_bins))
+    allocate(frequency(num_bins))
+
+    do i=1,num_bins
+        bins(i)=real(min_val)+(i-1)*bin_size
+        frequency(i)=0
+    end do
+
+    do i=1,num_bins
+        do j=1,10000
+            if (data_array(j)>=bins(i) .and. data_array(j)<bins(i)+bin_size) then
+                frequency(i)=frequency(i)+1
+            end if
+        end do
+    end do
+ 
+    do i=1,num_bins
+        bins(i)=bins(i)+bin_size/2
+    end do
+
+    open(unit=unit_num, file=path)
+    
+    do i=1,num_bins
+        write (unit_num,*) bins(i), " - ", frequency(i), " - ", frequency(i)/data_size
+    end do
+
+    close(unit_num)
+
+end subroutine graph_data
--- Module_1/test_file.f90
+++ Module_1/test_file.f90
@@ -1,38 +1,75 @@
 program large_number_trials
     implicit none
 
-    integer :: i, j        !iterating variable
+    integer :: k, l        !iterating variable
     real *8 :: sum_list(10000), rand_num(10000)
     real *8 :: sum_bigger_list(100000), rand_bigger_num(100000)
 
-    integer :: num_bins, min_val, max_val
-    real *8, allocatable :: bins(:), frequency(:)
-    real *8 :: bin_size 
+    !integer :: num_bins, min_val, max_val
+    !real, allocatable :: bins(:), frequency(:)
+    !real, parameter :: bin_size=2
 
     !!! question 1h
     !!! finding the sum of 10^4 trials 10^4 times
-    do i=1,10000
+    do k=1,10000
         call random_seed()
-        do j=1,10000
-            call random_number(rand_num(j))
+        do l=1,10000
+            call random_number(rand_num(l))
         end do
-        sum_list(i)=sum(rand_num)
+        sum_list(k)=sum(rand_num)
     end do
 
-    !!! code to construct bins and frequency for histogram
-    min_val=int(minval(sum_list)-bin_size)
-    max_val=int(maxval(sum_list)+1)
+    call graph_data(sum_list,10000,0.5,"file path here",5)
+    
+end program large_number_trials
 
-    if (mod(real(max_val),bin_size) /= 0.0) then
-        max_val=int(max_val-mod(real(max_val),bin_size)+2*bin_size)
+!!! code to construct bins and frequency for histogram
+subroutine graph_data(data_array, data_size, bin_size, path, unit_num)
+    implicit none
+
+    character(len=*), intent(in) :: path
+    integer :: i, j
+    integer :: num_bins, min_val, max_val
+    integer, intent(in) :: data_size, unit_num
+    real, allocatable :: bins(:), frequency(:)
+    real, intent(in) :: bin_size
+    real *8, intent(in) :: data_array(data_size)
+
+    min_val=int(minval(data_array)-bin_size)
+    max_val=int(maxval(data_array)+1.0)
+
+    if (mod(real(max_val-min_val),bin_size) /= 0.0) then
+        max_val=int(real(max_val)-mod(real(max_val-min_val),bin_size)+bin_size)
     end if
 
-    num_bins=int(max_val-min_val)
+    num_bins=int(real(max_val-min_val)/bin_size)
+
+    allocate(bins(num_bins))
+    allocate(frequency(num_bins))
 
     do i=1,num_bins
-        bins(i)=min_val+(i-1)*bin_size
+        bins(i)=real(min_val)+(i-1)*bin_size
+        frequency(i)=0
     end do
 
-    print *, bins
+    do i=1,num_bins
+        do j=1,10000
+            if (data_array(j)>=bins(i) .and. data_array(j)<bins(i)+bin_size) then
+                frequency(i)=frequency(i)+1
+            end if
+        end do
+    end do
+ 
+    do i=1,num_bins
+        bins(i)=bins(i)+bin_size/2
+    end do
 
-end program large_number_trials
+    open(unit=unit_num, file=path)
+    
+    do i=1,num_bins
+        write (unit_num,*) bins(i), " - ", frequency(i), " - ", frequency(i)/data_size
+    end do
+
+    close(unit_num)
+
+end subroutine graph_data
